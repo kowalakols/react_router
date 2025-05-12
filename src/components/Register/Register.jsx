d6b513ba4032f28d94cf1e09edae1ce6ac1eaf60
afole <koladeafo@gmail.com>
2025-05-12 12:35:20 +0100

    5th commit

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,10 @@
 import { Link, useNavigate, Navigate } from "react-router";
 import { useState, useContext } from "react";
-// import Spinner from "../Spinner/Spinner";
-// import { register } from "../../services/auth";
-// import { UserContext } from "../../contexts/UserContext";
+import { register } from "../../services/auth";
+import { UserContext } from "../../contexts/UserContext";
 
 export default function Register(){
-    // const { user } = useContext(UserContext)
+    const { user } = useContext(UserContext)
 
     const [formData, setFormData] = useState({
         email: '',
@@ -42,4 +41,46 @@ export default function Register(){
     if (user) {
         return <Navigate to="/" />
     }
-}
+    
+    return (
+      <section className="form-page">
+        <form onSubmit={handleSubmit} className="form">
+          <h1>Create an account</h1>
+  
+          {/* Email */}
+          <div className="input-control">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="Email" required onChange={handleChange} value={formData.email}/>
+            { error.email && <p className="error-message">{error.email}</p>}
+          </div>
+  
+          {/* Username */}
+          <div className="input-control">
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" placeholder="Username" required onChange={handleChange} value={formData.username} />
+            { error.username && <p className="error-message">{error.username}</p>}
+          </div>
+  
+          {/* Password */}
+          <div className="input-control">
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Password" required onChange={handleChange} value={formData.password} />
+            { error.password && <p className="error-message">{error.password}</p>}
+          </div>
+  
+          {/* Password Confirmation */}
+          <div className="input-control">
+            <label htmlFor="passwordConfirmation">Password Confirmation</label>
+            <input type="password" name="passwordConfirmation" id="passwordConfirmation" placeholder="Password Confirmation" required onChange={handleChange} value={formData.passwordConfirmation} />
+            { error.passwordConfirmation && <p className="error-message">{error.passwordConfirmation}</p>}
+          </div>
+  
+          <button type="submit">
+            { isLoading ? <Spinner /> : 'Register' }
+          </button>
+  
+          <small>Already have an account? <Link to="/login">Log back in</Link></small>
+        </form>
+      </section>
+    )
+  }
--- src/components/nav/navbar.jsx
+++ src/components/nav/navbar.jsx
@@ -7,9 +7,13 @@ export default function Navbar(){
       <div className='brand-logo'>
         <NavLink to="/">🚀</NavLink>
       </div>
-      <nav>
+      <nav className="functions">
         <NavLink to="/activities">activities</NavLink>
         <NavLink to="/activities/new">create activity</NavLink>
+      </nav>
+      <nav className="authentication">
+        <NavLink to="/register">create an account</NavLink>
+        <NavLink to="/login">login</NavLink>
       </nav>
     </header>
   )
